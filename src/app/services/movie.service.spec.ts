85fe20c3e5567ec21d8040648618bc56bfc6338b
Erick Daniel Teixeira Vier <erixkat10@gmail.com>
2024-11-21 13:24:49 -0300

    Update movie.service.spec.ts

--- src/app/services/movie.service.spec.ts
+++ src/app/services/movie.service.spec.ts
@@ -1,16 +1,40 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
-import { MovieService } from './movie.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  private favorites: any[] = [];
 
-describe('MovieService', () => {
-  let service: MovieService;
+  constructor() {}
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MovieService);
-  });
+  // Metodo para pesquisar filmes
+  searchMovies(title: string): Observable<any> {
+    // Simulação de chamada HTTP retornando um observable
+    return of({ Search: [{ Title: 'Avengers', Year: '2012', imdbID: 'tt0848228', Type: 'movie' }] });
+  }
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  // Metodo para obter detalhes do filme
+  getMovieDetails(imdbID: string): Observable<any> {
+    // Simulação de chamada HTTP retornando um observable
+    return of({ Title: 'Avengers', Year: '2012', imdbID: 'tt0848228', Plot: 'Plot do filme' });
+  }
+
+  // Metodo para adicionar aos favoritos
+  addToFavorites(movie: any): Observable<any[]> {
+    this.favorites.push(movie);
+    return of(this.favorites); // Retornando a lista atualizada de favoritos como Observable
+  }
+
+  // Metodo para remover dos favoritos
+  removeFromFavorites(imdbID: string): Observable<any[]> {
+    this.favorites = this.favorites.filter(movie => movie.imdbID !== imdbID);
+    return of(this.favorites); // Retornando a lista atualizada de favoritos como Observable
+  }
+
+  // Metodo para obter todos os favoritos
+  getFavorites(): Observable<any[]> {
+    return of(this.favorites); // Retornando a lista de favoritos como Observable
+  }
+}
